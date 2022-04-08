91080d2a31e5139f09ba2f8b9156e860e9243234
MadeByMM <marcm6000@hotmail.com>
2022-04-08 17:36:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tutorapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.tutorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize Bottom Navigation View.
+        BottomNavigationView navView = findViewById(R.id.bottomNav_view);
+
+        //Pass the ID's of Different destinations
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_home, R.id.navigation_toolbox, R.id.navigation_terms, R.id.navigation_bookshelf )
+                .build();
+
+        //Initialize NavController.
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+
+    }
+
+
+}
